3dba46ccafce0fa5e703435a44f02f15a99b79c0
himalkafle11 <himalkafle11@gmail.com>
2023-03-13 15:15:26 +0545

    added delete feature

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,14 +1,31 @@
 import React from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 const WorkoutDetails = ({ workout }) => {
-  const dateTimeString = workout.createdAt;
-  const date = new Date(dateTimeString);
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1; // months are zero-indexed, so we need to add 1
-  const day = date.getDate();
-  const formattedDate = `${year}-${month.toString().padStart(2, "0")}-${day
-    .toString()
-    .padStart(2, "0")}`;
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
+  const formattedDate = () => {
+    const dateTimeString = workout.createdAt;
+    const date = new Date(dateTimeString);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `${year}-${month.toString().padStart(2, "0")}-${day
+      .toString()
+      .padStart(2, "0")}`;
+  };
+
   return (
     <div className="flex flex-col border bg-white shadow-lg p-4">
       <h4 className="text-blue-700 font-extrabold uppercase">
@@ -28,9 +45,10 @@ const WorkoutDetails = ({ workout }) => {
         <p></p>
         <p>
           <strong>Date : </strong>
-          {formattedDate}
+          {formattedDate()}
         </p>
       </div>
+      <span onClick={handleClick}>Delete</span>
     </div>
   );
 };
